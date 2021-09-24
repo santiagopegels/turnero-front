34bf27207ad372c5dfca3ea56250ccf6b9ae19b7
Santiago Pegels <santiagopegels@gmail.com>
2021-09-24 15:50:15 -0300

    send token to socket.io

--- src/context/socket.js
+++ src/context/socket.js
@@ -1,5 +1,10 @@
 import React from 'react'
 import socketio from "socket.io-client";
 
-export const socket = socketio(process.env.REACT_APP_SOCKET);
+export const socket = socketio(process.env.REACT_APP_SOCKET, {
+    auth: {
+        token: localStorage.getItem('token')
+    },
+});
+
 export const SocketContext = React.createContext();
